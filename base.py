c0a5097abc89980a67ee95a8c256866f07662896
MukaVPS <samuca.victorp14@gmail.com>
2025-10-24 17:03:31 -0300

    Update base.py

--- base.py
+++ base.py
@@ -2,18 +2,15 @@
 import pandas as pd
 
 # Base de dados
-df = pd.read_csv('vgsales.csv')
-print(df)
-
-# Limpeza de dados
+df = pd.read_csv('vgsales.csv'))
 
 # Top 10 por vendas globais
 top10 = df.sort_values('Global_Sales', ascending=False).head(10)
-print("\nTop 10 jogos por Global_Sales:")
-print(top10[['Name','Platform','Year','Genre','Global_Sales']].to_string(index=False))
+
+# Top 10 por vendas globais com as Menores pontuações
+top10_negative = df.sort_values('Global_Sales', ascending=True).head(10)
 
 # Pontuação Total dos Gêneros dos jogos
 gen = df.groupby('Genre')['Global_Sales'].sum()
-print(gen)
 
 # Visualização
