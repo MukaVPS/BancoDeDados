a6b6688434092e3cd110ee13526e47f729b1e124
MukaVPS <samuca.victorp14@gmail.com>
2025-10-24 17:00:16 -0300

    Update base.py

--- base.py
+++ base.py
@@ -12,4 +12,8 @@ top10 = df.sort_values('Global_Sales', ascending=False).head(10)
 print("\nTop 10 jogos por Global_Sales:")
 print(top10[['Name','Platform','Year','Genre','Global_Sales']].to_string(index=False))
 
+# Pontuação Total dos Gêneros dos jogos
+gen = df.groupby('Genre')['Global_Sales'].sum()
+print(gen)
+
 # Visualização
