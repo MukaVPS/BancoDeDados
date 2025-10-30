b3b8106dfc26bddb978d99d54cce197e044aec33
MukaVPS <samuca.victorp14@gmail.com>
2025-10-30 15:10:29 -0300

    Update VisualizaçãoTeste.py

--- VisualizaçãoTeste.py
+++ VisualizaçãoTeste.py
@@ -1,18 +1,1 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
-import sys
 
-# Carregar a base de dados
-df = pd.read_csv('vgsales.csv')
-top10 = pd.read_csv('top10_vendas_globais.csv')
-top10_negative = pd.read_csv('top10_menores_vendas_globais.csv')
-regions = pd.read_csv('vendas_por_regiao.csv')
-
-# ----------------------------
-# Top 10 Mais Vendidos
-# ----------------------------
-top10.plot(x='Name',y='Global_Sales',kind='bar')
-
-# Ajustar espaçamento e exibir
-plt.show()
