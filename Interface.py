ffa8145ca474afffed8d267f15afb70061036907
MukaVPS <samuca.victorp14@gmail.com>
2025-10-30 15:58:46 -0300

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -12,9 +12,15 @@ def top10_menores_vendas_globais():
     plt.subplots_adjust(bottom=0.35)
     plt.show()
 
+def top5_empresas():
+    top5empresas.plot(figsize=(10,6), x='Publisher', y='Global_Sales', kind='bar')
+    plt.subplots_adjust(bottom=0.35)
+    plt.show()
+
 # Bases de dados
 top10 = pd.read_csv('top10_vendas_globais.csv')
 top10negative = pd.read_csv('top10_menores_vendas_globais.csv')
+top5empresas = pd.read_csv('top5_empresas_maiores_vendas.csv')
 
 # Interface
 windows = tk.Tk()
@@ -23,11 +29,15 @@ windows.title("Vendas de Jogos")
 
 # Botão de top10 vendas globais
 botaotop10 = tk.Button(windows, text="Top 10 de Vendas Globais", command=top10_vendas_globais)
-botaotop10.pack(pady = 20)
+botaotop10.pack(pady = 10)
 
 # Botão de top10 vendas globais
 botaotop10negative = tk.Button(windows, text="Top 10 Menores Vendas Globais", command=top10_menores_vendas_globais)
-botaotop10negative.pack(pady = 5)
+botaotop10negative.pack(pady = 10)
+
+# Botão de top10 vendas globais
+botaotop5empresas = tk.Button(windows, text="Top 5 empresas com maiores vendas", command=top5_empresas)
+botaotop5empresas.pack(pady = 10)
 
 # Executar Interface
 windows.mainloop()
